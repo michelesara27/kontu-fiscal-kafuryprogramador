693ca6d4e21dd5ad24d286f6adcf74ead86c6e63
michelesara27 <michelesara27@gmail.com>
2025-08-20 21:35:00 -0300

    Initial commit

--- src/services/companiesService.ts
+++ src/services/companiesService.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_BASE_URL = 'http://kontubd.kafuryprogramador.com.br/';
 
-export const companyAPI = {
+export const companiesService = {
   createCompany: (data: CompanyFormData) => 
     axios.post(`${API_BASE_URL}companies.php`, data),
   
